b8442cdbf5618e5db6a90b3c6540f56590aa2d82
Nicolas <nicolas.gehin@pixelea.fr>
2025-01-08 14:16:05 +0100

    Modification de l'auteur

--- up-bk-slick-slider.php
+++ up-bk-slick-slider.php
@@ -5,7 +5,7 @@
  * Requires at least: 6.1
  * Requires PHP:      7.4
  * Version:          1.1.0
- * Author:           UP
+ * Author:           GEHIN Nicolas
  * License:          GPL-2.0-or-later
  * License URI:      https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:      up-bk-slick-slider
